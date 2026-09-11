5577448dd81f27a11577a50d5f022fdc206b5d17
komal-kumari-040908 <shitalkumari1203@gmail.com>
2026-09-11 19:38:58 +0530

    Add program to calculate average of two numbers

---
+++ prct3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    double avg;
+    cout<<"enter value of num1:";
+    cin>>num1;
+    cout<<"enter value of num2:";
+    cin>>num2;
+    avg=(num1+num2)/2;
+    cout<<"average of two numbers:"<<avg<<endl;
+    return 0;
+}
