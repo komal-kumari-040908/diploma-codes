a486052afa566c65c9d4195d1b53ae166fd2851d
komal-kumari-040908 <shitalkumari1203@gmail.com>
2026-09-11 17:41:42 +0530

    Add files via upload

---
+++ pct1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"enter value of x:";
+    cin>>x;
+    y=5*x-5;
+    cout<<"value of y is:"<<y<<endl;
+    return 0;
+}        
