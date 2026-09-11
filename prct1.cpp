97f00acd69a8474ecf9f896bd3fa346e0a672399
komal-kumari-040908 <shitalkumari1203@gmail.com>
2026-09-11 19:38:09 +0530

    Add basic number comparison program in C++

---
+++ prct1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter number:";
+    cin>>num;
+    if(num>10)
+    {
+        cout<<"Hi";
+    }
+    else
+    {
+        cout<<"Bye";
+    }
+    return 0;
+}
